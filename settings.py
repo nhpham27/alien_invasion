9fdad2715c79003ee2ac95376d8bc598b76f985e
Nguyen Hoa Pham <npham30@uic.edu>
2021-07-27 19:28:19 -0500

    initial commit

---
+++ settings.py
@@ -0,0 +1,10 @@
+class Settings:
+    """A class to store settings for Alien Invasion game"""
+    def __init__(self) -> None:
+        """Initialize the game setting"""
+        # Screen setting
+        self.screen_width = 600
+        self.screen_height = 400
+        self.bg_color = (230, 230, 230)
+        
+         
